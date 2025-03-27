5dfb2b05cb5f68b68b88769bcd4f747d62ab22b7
Pagão <gabrielpaganinibarroso@hotmail.com>
2025-03-27 10:22:11 -0300

    feat: created users model and added auditioning annotation to the main class

---
+++ src/main/java/com/backend/pilates/model/User.java
@@ -0,0 +1,55 @@
+package com.backend.pilates.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.security.Timestamp;
+import java.time.Instant;
+
+@Entity
+@Table(name = "db_users")
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false)
+    @NotBlank
+    private String first_name;
+    @Column(nullable = false)
+    @NotBlank
+    private String last_name;
+    @Column(nullable = false, unique = true)
+    @NotBlank
+    @Email
+    private String email;
+    @Column(nullable = false)
+    @NotBlank
+    private String password;
+    @ManyToOne
+    @JoinColumn(name = "role_id", nullable = false)
+    private Roles role_id;
+    @CreatedDate
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
+    @Column(nullable = false, updatable = false)
+    //como são para fins de auditoria, horários em UTC
+    private Instant created_at;
+    @LastModifiedDate
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
+    @Column(nullable = false)
+    //como são para fins de auditoria, horários em UTC
+    private Instant updated_at;
+}
