c6d58fb60506abc72a009369579c54037e6ce103
Pagão <gabrielpaganinibarroso@hotmail.com>
2025-03-31 15:17:41 -0300

    fix: schema created for the roles dto request

--- src/main/java/com/backend/pilates/dtos/request/RolesRequestDTO.java
+++ src/main/java/com/backend/pilates/dtos/request/RolesRequestDTO.java
@@ -5,9 +5,12 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
 public record RolesRequestDTO(
-                              @Schema(description = "Papel do usuário no sistema",
+                              @Schema(description = "Tipo do perfil no sistema",
                                       example = "ROLE_ADMIN",
-                                      allowableValues = {"ROLE_ADMIN", "ROLE_PROFESSOR"})
+                                      allowableValues = {"ROLE_ADMIN", "ROLE_PROFESSOR"},
+                                      implementation = String.class,
+                                      type = "string")
                               RolesEnum name,
+                              @Schema(description = "Descrição do perfil")
                               @NotBlank String description) {
 }
