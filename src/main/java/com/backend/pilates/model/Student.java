be067bc03094f57dafd9990931e93dd12aeff85a
Gabriel Paganini <56180950+Pagaoo@users.noreply.github.com>
2025-04-01 14:45:55 -0300

    Merge pull request #1
    * feat: created student entity

--- src/main/java/com/backend/pilates/model/Student.java
+++ src/main/java/com/backend/pilates/model/Student.java
@@ -1,4 +1,46 @@
 package com.backend.pilates.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "db_students")
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank
+    @Column(nullable = false, length = 30)
+    private String first_name;
+    @NotBlank
+    @Column(nullable = false, length = 30)
+    private String last_name;
+    @NotBlank
+    @Column(nullable = false, length = 15, unique = true)
+    private String phone;
+    @Column(nullable = false)
+    private Boolean is_active;
+    @CreatedDate
+    @Column(nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
+    private Instant created_at;
+    @LastModifiedDate
+    @Column(nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
+    private Instant updated_at;
 }
