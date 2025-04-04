4c6301ca4b659b54f04975b9e87ba6459674d15d
Pagão <gabrielpaganinibarroso@hotmail.com>
2025-04-04 18:11:51 -0300

    feat: days of the week entity created

---
+++ src/main/java/com/backend/pilates/model/DaysOfTheWeek.java
@@ -0,0 +1,27 @@
+package com.backend.pilates.model;
+
+import com.backend.pilates.enums.DaysOfWeekEnum;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "day_of_the_week")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DaysOfTheWeek {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    @Schema(description = "Dias das semanas que tem aula",
+            example = "SEGUNDA",
+            allowableValues = {"SEGUNDA", "TERCA", "QUARTA", "QUINTA", "SEXTA"})
+    private DaysOfWeekEnum dayOfTheWeek;
+}
