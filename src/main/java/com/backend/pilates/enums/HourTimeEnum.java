6842eaa78a2df55825daa503fece73eb2c80b73a
Pagão <gabrielpaganinibarroso@hotmail.com>
2025-04-04 17:59:17 -0300

    feat: enums to display hour time and day of the week for the classes created

--- src/main/java/com/backend/pilates/enums/HourTimeEnum.java
+++ src/main/java/com/backend/pilates/enums/HourTimeEnum.java
@@ -1,4 +1,24 @@
 package com.backend.pilates.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum HourTimeEnum {
+    OITO_HORAS("08:00"),
+    NOVE_HORAS("09:00"),
+    DEZ_HORAS("10:00"),
+    TREZE_HORAS("13:00"),
+    QUATORZE_HORAS("14:00"),
+    QUINZE_HORAS("15:00"),
+    DEZESSEIS_HORAS("16:00"),
+    DEZESETE_HORAS("17:00"),
+    DEZOITO_HORAS("18:00"),
+    DEZENOVE_HORAS("19:00"),
+    VINTE_HORAS("20:00");
+
+    private final String displayHourTime;
+
+    HourTimeEnum(String displayHourTime) {
+        this.displayHourTime = displayHourTime;
+    }
 }
