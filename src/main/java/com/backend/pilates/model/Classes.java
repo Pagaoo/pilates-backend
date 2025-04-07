a2b4b781ab56738e07bff3a8a7368218fa1d4f67
Pagão <gabrielpaganinibarroso@hotmail.com>
2025-04-07 18:13:51 -0300

    feat: classes model created

--- src/main/java/com/backend/pilates/model/Classes.java
+++ src/main/java/com/backend/pilates/model/Classes.java
@@ -1,4 +1,43 @@
 package com.backend.pilates.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "classes")
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Classes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "professor_id", nullable = false)
+    private Professor professor;
+    @ManyToOne
+    @JoinColumn(name = "day_of_the_week_id", nullable = false)
+    private DaysOfTheWeek day_of_the_week;
+    @ManyToOne
+    @JoinColumn(name = "hour_time_id", nullable = false)
+    private HourTime hour_time;
+    @CreatedDate
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
+    @Column(nullable = false, updatable = false)
+    private Instant created_at;
+    @LastModifiedDate
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
+    @Column(nullable = false)
+    private Instant updated_at;
 }
