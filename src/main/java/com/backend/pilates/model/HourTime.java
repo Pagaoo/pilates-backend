02a5956a818eb8e9b998dd8385bb9786874c4cf0
Pagão <gabrielpaganinibarroso@hotmail.com>
2025-04-07 17:50:38 -0300

    feat: hour time model created

---
+++ src/main/java/com/backend/pilates/model/HourTime.java
@@ -0,0 +1,26 @@
+package com.backend.pilates.model;
+
+import com.backend.pilates.enums.HourTimeEnum;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "hour_time")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class HourTime {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    @Schema(description = "Horas do dia que tem aula",
+    example = "NOVE_HORAS")
+    private HourTimeEnum hourTime;
+}
