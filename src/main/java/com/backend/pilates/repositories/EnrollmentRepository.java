fc1bf51e850065776a06bcb5e832f0e70cd154d1
Pagão <gabrielpaganinibarroso@hotmail.com>
2025-05-17 08:55:55 -0300

    feat: created delete student from class method

--- src/main/java/com/backend/pilates/repositories/EnrollmentRepository.java
+++ src/main/java/com/backend/pilates/repositories/EnrollmentRepository.java
@@ -3,9 +3,10 @@ package com.backend.pilates.repositories;
 import com.backend.pilates.model.Classes;
 import com.backend.pilates.model.Enrollments;
 import com.backend.pilates.model.Student;
-import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EnrollmentRepository extends JpaRepository<Enrollments, Long> {
+    Enrollments findByStudentAndClasses(Student student, Classes classes);
+
     boolean existsByStudentAndClasses(Student student, Classes classes);
 }
